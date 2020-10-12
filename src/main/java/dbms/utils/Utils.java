a9cc7209a628e8b7bed151984f80321c6af2f4f4
Raluca Boyte <ralucaboyte@gmail.com>
2020-10-12 17:15:21 +0300

    Added folder and directory creation upon adding index

--- src/main/java/dbms/utils/Utils.java
+++ src/main/java/dbms/utils/Utils.java
@@ -5,13 +5,17 @@ import com.google.gson.JsonParser;
 import dbms.domain.Database;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.text.ParseException;
 import java.util.List;
+import java.util.Objects;
 
 public class Utils {
 
@@ -20,6 +24,45 @@ public class Utils {
             objectMapper.writeValue(file, databaseList);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static String createFolderLocation(String databaseName, String tableName) {
+
+        return "src/main/resources/" + databaseName + "_" + tableName;
+    }
+
+    private static String createFileLocation(String folderName, String filename) {
+
+        return folderName + "/" + filename + ".ind";
+    }
+
+    public static void createFile(String filename, String databaseName, String tableName) {
+
+        String folderUploadLocation = createFolderLocation(databaseName, tableName);
+        String fileUploadLocation = createFileLocation(folderUploadLocation, filename);
+
+        Path newUploadLocation = Paths.get(folderUploadLocation);
+        createDirectories(newUploadLocation);
+
+        File f1 = new File(fileUploadLocation);
+        try {
+            f1.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void createDirectories(Path uploadLocation) {
+
+        if (!Files.exists(uploadLocation)) {
+            try {
+                Files.createDirectories(uploadLocation);
+            } catch (IOException e) {
+                System.out.println("++++++++create directories failed++++++++");
+            }
         }
     }
 
